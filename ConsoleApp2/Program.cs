fd28228b52fe2ecdb32e6495de12818a950ae92d
heidi <baowenhuangnts@cstcollege.ca>
2023-01-31 22:30:41 -0500

    test

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string message = "Hello World! This is Heidi";
+            Console.WriteLine(message);
+            Console.ReadLine();
+        }
+    }
+}
