9a1f896a2018e3d8066ea4b771fd89a6853e1eb9
heidi <baowenhuangnts@cstcollege.ca>
2023-02-01 21:57:08 -0500

    test

---
+++ day2/CstConsoleday2/Consolefunction/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolefunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //SayHello();
+            //Console.ReadKey();
+
+
+            //void SayHello()
+            //{
+            //    Console.WriteLine("Welcome to CST college.!!");
+            //}
+
+            string mystr = "Tom";
+            string resultStr;
+
+            resultStr= SayHello(mystr);
+            Console.WriteLine(resultStr);
+
+            Console.WriteLine(SayHello());
+            Console.ReadLine();
+
+
+// define a method
+            string SayHello(string str="Joy") 
+            {
+
+                string message="Welcome to CST college:" + str;
+                return message;
+            }
+
+        }
+    }
+  }
---
+++ day2/CstConsoleday2/CstConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿// author Cst Heidi
+// date: Feb 1 2023
+/*
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //short a;
+            //int b;
+            //double c;
+
+            //a = 10;
+            //b = 30;
+            //c = a + b;
+            int x = 42;
+            Console.WriteLine(x++);
+            Console.WriteLine(++x);
+
+
+            //Console.WriteLine("a={0},b={1},c={2}",a,b,c);
+            Console.ReadKey();
+        }
+    }
+}
