7817b72199c2db241c7a33bd62e765a80f0b8b32
heidi <baowenhuangnts@cstcollege.ca>
2023-02-02 21:27:52 -0500

    test

---
+++ day3/CstDay3/Cststatements/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cststatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("please enter one integer number");
+            int nums =Convert.ToInt32(Console.ReadLine());
+            if (nums%2==0)
+            {
+                Console.WriteLine("the number is even");
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("the number is odd");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ day3/CstDay3/CstwitchApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CstwitchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * print out student mark according 
+             * score
+             */
+
+            Console.WriteLine("please put student's score (integer between 0~100)");
+
+            int points = Convert.ToInt32(Console.ReadLine());
+            #region
+            // start switch
+            switch (points / 10)
+            {
+
+                case 10:
+                    Console.WriteLine("great!");
+                    break;
+                case 9:
+                    Console.WriteLine("perfect");
+                    break;
+                case 8:
+                    Console.WriteLine("good");
+                    break;
+                case 7:
+                    Console.WriteLine("pass");
+                    break;
+                case 6:
+                    Console.WriteLine("pass");
+                    break;
+
+                default:
+                    Console.WriteLine("failed");
+                    break;
+
+
+            }
+
+            //end switch
+            #endregion
+
+        
+        }
+    }
+}
